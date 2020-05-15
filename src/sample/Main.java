3a7d2889e181d9f71f88fdcd1a18bf864b601ed9
Mohamed Essam Abdelfatah <imessam1998@gmail.com>
2020-05-15 07:52:12 +0200

    Update from IDE 1

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,25 +4,10 @@
 
 package sample;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
-public class Main extends Application {
-
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("scene1.fxml"));
-        primaryStage.setTitle("Memory Manager");
-        primaryStage.setScene(new Scene(root, 600, 400));
-        SecondScene.getActiveStage(primaryStage);
-        primaryStage.show();
-    }
+public class Main {
 
 
     public static void main(String[] args) {
-        launch(args);
+        MemoryManager.main(args);
     }
 }
